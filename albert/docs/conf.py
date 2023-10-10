8660f7d59c5d0e973f4adb3428d711e98f60678b
Heber Scachetti <heber.augusto@gmail.com>
2023-10-10 16:10:14 -0300

    Delete albert/docs directory

--- albert/docs/conf.py
+++
@@ -1,8 +0,0 @@
-# conf.py
-
-project = 'Biblioteca Albert'
-author = 'Heber Augusto'
-master_doc = 'index'
-extensions = [
-    'sphinx.ext.autodoc',
-]
--- albert/docs/index.rst
+++
@@ -1,2 +0,0 @@
-.. Meu Projeto Documentation documentation master file, created by
-   sphinx-quickstart on Tue Oct 10 10:00:00 2023.
