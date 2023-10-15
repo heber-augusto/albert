b5e1eda7848bcaa31771f2cacdcc49035c2bd918
Heber Augusto Scachetti <heber.augusto@gmail.com>
2023-10-15 12:22:24 +0000

    alteracao na aplicação flask, para evitar carga do modelo a cada chamada do endpoint e disponibilizar a api rest na pagina inicial

--- albert/jobtypes/templates/inference/source/app.py
+++ albert/jobtypes/templates/inference/source/app.py
@@ -1,25 +1,54 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask_restful import Resource, Api
 from flasgger import Swagger
+from code import perform_inference, load_generative_model
 
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
 
-class HelloWorld(Resource):
-    def get(self):
+# Variável global para armazenar o modelo carregado
+generative_model = None
+
+class Inference(Resource):
+    def post(self):
         """
-        Exemplo de endpoint de saudação.
+        Execute a inferência com um modelo de machine learning.
         ---
         tags:
-          - saudação
+          - inferência
+        parameters:
+          - name: input_content
+            in: formData
+            type: string
+            required: true
+            description: Conteúdo de entrada para inferência
         responses:
           200:
-            description: Mensagem de saudação
+            description: Resultado da inferência
         """
-        return {'message': 'Olá, mundo!'}
+        # Execute a inferência
+        input_content = request.form['input_content']
+        result = perform_inference(input_content, generative_model)
 
-api.add_resource(HelloWorld, '/')
+        return result
+
+
+def initialize_app():
+    global generative_model
+    if generative_model is None:
+        # Realize o carregamento do modelo, por exemplo, usando seu código atual
+        generative_model = load_generative_model()
+
+
+# Rota para a página inicial que redireciona para a documentação do Swagger
+@app.route('/')
+def index():
+    return redirect('/apidocs')
+
+
+api.add_resource(Inference, '/inference')
 
 if __name__ == '__main__':
+    initialize_app()
     app.run(debug=True)
