281be0d7149de0c3943c6c14fa70a56c5d6ce73a
Heber Augusto Scachetti <heber.augusto@gmail.com>
2023-10-16 10:08:16 +0000

    alteração em arquivos de template de código para evitar erros durante execucao de testes

--- albert/jobtypes/templates/finetuning/deploy/deploy.sh
+++ albert/jobtypes/templates/finetuning/deploy/deploy.sh
@@ -1,5 +1,4 @@
 # minikube start --force --cpus=1
 eval $(minikube -p minikube docker-env)
-docker build -t albert-inference-job-minikube:latest ../source
+docker build -t albert-finetuning-job-minikube:latest ../source
 kubectl apply -f minikube.yaml
-kubectl port-forward svc/albert-inference-service 5000:5000
