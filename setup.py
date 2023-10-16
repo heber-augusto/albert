6d36089178890fcbc394694bcfb92caabf386c5d
Heber Augusto Scachetti <heber.augusto@gmail.com>
2023-10-16 10:32:23 +0000

    altera versão para criar uma nova lib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="albert",
-    version="0.0.1.dev3",
+    version="0.0.1.dev4",
     packages=find_packages(),
     package_data={
         "albert.jobtypes.templates": ["*.*"]
