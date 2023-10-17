a2e583f6c6a62c8af482617267998e9f60008fa1
Heber Augusto Scachetti <heber.augusto@gmail.com>
2023-10-17 02:44:05 +0000

    atualiza versao

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="albert",
-    version="0.0.1.dev4",
+    version="0.0.1.dev5",
     packages=find_packages(),
     package_data={
         "albert.jobtypes.templates": ["*.*"]
